1e1102e2149dabf824b47d89314ce2d98a1138fe
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2022-10-27 16:18:20 +0200

    Updated bundle

--- peptides/dist/peptide-viewer.bundle.js
+++ peptides/dist/peptide-viewer.bundle.js
@@ -3975,7 +3975,7 @@ function initNXDivs() {
         sequences.forEach(function (o) {
             if (o.uniqueName === isoName) {
                 currentSeq = o.sequence;
-                ft = new FeatureViewer(currentSeq, "#featureViewer", {
+                ft = new createFeature(currentSeq, "#featureViewer", {
                     showAxis: true,
                     showSequence: true,
                     brushActive: true,
